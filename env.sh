fab3d6855438a080ec3237b6cddf095b35bf31e8
gari30 <gariyoshi0630@gmail.com>
2021-06-19 18:07:32 +0900

    ビルド時に必要な環境変数の定義シェルスクリプトの追加

---
+++ env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export KERNEL=kernel
+export ARCH=arm
+export CROSS_COMPILE=/mnt/4tb/raspberrypi_kernel/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin/arm-linux-gnueabihf-
