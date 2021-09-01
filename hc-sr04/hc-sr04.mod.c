f7d199f358195037bd7273d48baf84279879b9fb
gari30 <gariyoshi0630@gmail.com>
2021-09-02 03:39:21 +0900

    rmみすって全ブッパしたのをだいたい復旧したのでコミット

--- hc-sr04/hc-sr04.mod.c
+++ hc-sr04/hc-sr04.mod.c
@@ -25,12 +25,19 @@ MODULE_INFO(retpoline, "Y");
 static const struct modversion_info ____versions[]
 __used __section(__versions) = {
 	{ 0xe338ec46, "module_layout" },
-	{ 0x6091b333, "unregister_chrdev_region" },
+	{ 0xfe990052, "gpio_free" },
+	{ 0x1926e08c, "class_destroy" },
+	{ 0xcf2eb6cf, "device_create" },
 	{ 0x2ae31514, "cdev_del" },
 	{ 0x550754f6, "__class_create" },
+	{ 0x6091b333, "unregister_chrdev_region" },
 	{ 0xdb4e9b54, "cdev_add" },
 	{ 0xe91a98fb, "cdev_init" },
 	{ 0xe3ec2f2b, "alloc_chrdev_region" },
+	{ 0x8ceb1781, "gpiod_get_raw_value" },
+	{ 0x5c5e06b4, "gpiod_direction_input" },
+	{ 0x3212d7f6, "gpio_to_desc" },
+	{ 0x47229b5c, "gpio_request" },
 	{ 0xc5850110, "printk" },
 	{ 0xb1ad28e0, "__gnu_mcount_nc" },
 };
@@ -38,4 +45,4 @@ __used __section(__versions) = {
 MODULE_INFO(depends, "");
 
 
-MODULE_INFO(srcversion, "8ACAB3AC44FCBB849C4BB51");
+MODULE_INFO(srcversion, "28A579BECDE9301AF6334CA");
