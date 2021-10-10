86b7d70f221ba5b6a3dccfe6dd77536c3bea41c1
gari30 <gariyoshi0630@gmail.com>
2021-10-11 00:15:56 +0900

    errnoを表示するように変更

--- test_hc-sr04/test_hc-sr04.c
+++ test_hc-sr04/test_hc-sr04.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <errno.h>
 #include <fcntl.h>
+#include <string.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
 
@@ -14,13 +16,18 @@
 
 int main (int argc, char *argv[]) {
   int fd;
+  int err;
   drv_rq_t rq;
 
   printf("%s ver.%d.%d\n", argv[0], TEST_HCSR04_MAJ_VER, TEST_HCSR04_MIN_VER);
 
+  errno = 0;
   fd = open(DEVICE_FILE, O_RDWR);
+  err = errno;
+
   if (0 > fd) {
     fprintf(stderr, "Can't open " DEVICE_FILE "\n");
+    fprintf(stderr, "errno: %s\n", strerror(err));
     exit(1);
   }
   if (0 > ioctl(fd, GPIO_HCSR04_EXEC_MEASURE_DISTANCE, &rq)) {
